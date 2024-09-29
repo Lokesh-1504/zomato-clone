272fe5e13ae37e807de53f7d9f9552a0b258d095
Lokesh Panjiar <126404063+Lokesh-1504@users.noreply.github.com>
2024-09-29 20:25:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,12 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 const corsOptions = {
-  origin: "https://zomato-typeface.netlify.app/",
-  "Content-Type": "Authorization",
+  origin: "*", // Allow any origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
-  optionSuccessStatus: 200,
+  optionsSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 
 dotenv.config();
